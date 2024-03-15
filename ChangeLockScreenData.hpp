a404fc46df5a7f78993ca2f8e70997f687ae413c
CarrieForle <53133715+CarrieForle@users.noreply.github.com>
2024-03-15 11:47:54 +0800

    Replace hard-coded path to env variable

--- ChangeLockScreenData.hpp
+++ ChangeLockScreenData.hpp
@@ -25,12 +25,15 @@ struct ChangeLockScreenData
     ChangeLockScreenData(HWND& hwnd) : main_hwnd(hwnd), has_locked(false), tray_menu(CreatePopupMenu()),
                              random_gen(std::random_device{}()),
                              alignment(GetSystemMetrics(SM_MENUDROPALIGNMENT) == 0 ? TPM_LEFTALIGN : TPM_RIGHTALIGN),
-                             root(LR"(C:\Users\carri\Pictures\slideshow_lockscreen\)"),
                              last_file(L"last_file.txt"),
                              ext(L"png"),
                              current_file(L"current." + ext)
 
     {
+        wchar_t *home_path;
+        GetEnvironmentVariable(L"USERPROFLE", home_path, 256);
+        root = std::filesystem::path(home_path) / LR"(Picture/slideshow_lockscreen)";
+        
         if (!std::filesystem::exists(root / last_file))
         {
             std::ofstream tmp{root / last_file};
