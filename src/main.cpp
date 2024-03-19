bb11a314f6307da3113b4235b425a912f3e1f611
CarrieForle <53133715+CarrieForle@users.noreply.github.com>
2024-03-19 17:45:43 +0800

    Added prevent 2+ instances of daemon running with mutex

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,20 @@
 #include "Log.hpp"
 #include <windows.h>
 #include <iostream>
+#include <mutex>
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, wchar_t *pCmdLine, int nCmdShow)
 {
+
+    // Prevent 2 instances of daemon running.
+
+    HANDLE single_instance_mutex = CreateMutex(NULL, TRUE, L"SINGLE INSTANCE");
+
+    if (GetLastError() == ERROR_ALREADY_EXISTS)
+    {
+        return 0;
+    }
+
     ChangeLockscreenDaemon daemon;
     if (!daemon.Create(L"CF Lockscreen image changer")) {
         return ErrorChangeLockscreen::build_daemon;
@@ -22,6 +33,8 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, wchar_t *pCmdL
         TranslateMessage(&msg);
         DispatchMessage(&msg);
     }
+
+    ReleaseMutex(single_instance_mutex);
 
     return 0;
 }
