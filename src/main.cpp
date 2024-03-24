c13aef0554ab9ce241b07b78f1a2a4f2a7ad0e52
CarrieForle <53133715+CarrieForle@users.noreply.github.com>
2024-03-24 15:46:24 +0800

    remove unnecessary things

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,10 @@
 #include "ChangeLockscreenDaemon.hpp"
-#include "ParsedData.hpp"
-#include "CommandLineParser.hpp"
-#include "ParserException.hpp"
 #include <windows.h>
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, wchar_t *pCmdLine, int nCmdShow)
 {
     // Prevent 2 instances of daemon running.
     HANDLE single_instance_mutex = CreateMutex(NULL, TRUE, L"SINGLE INSTANCE");
-
-    try {
-        ParsedData pd = CommandLineParser::Parse(pCmdLine);
-    } catch(ParserException& e) {
-        std::wcout << e.what() << "\n";
-
-        return 0;
-    }
 
     if (GetLastError() == ERROR_ALREADY_EXISTS)
     {
